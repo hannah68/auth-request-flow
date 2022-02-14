4ce4e48d8fdc031c5522eea1d27205ce11ac48ff
hannah68 <reihaneh.naderi1989@gmail.com>
2022-02-14 15:00:55 +0000

    requirement1,2 are done

--- src/router.js
+++ src/router.js
@@ -13,12 +13,35 @@ const mockUser = {
     }
 };
 
-router.post('/login', (req, res) => {
 
+const secretKey = "thisismysecretkey";
+
+//1. The client sends a request containing login credentials to the server.
+//2. The server checks the credentials are correct and sends back a token.(The server creates an access token for the user and sends it back to the client);
+
+router.post('/login', (req, res) => {
+    const { username } = req.body;
+
+    if(mockUser.username !== username){
+        res.status(401).send("Unauthorized");
+    }
+    const token = jwt.sign(username, secretKey);
+    // console.log(token);
+    res.json({tokenKey: token});
 });
 
-router.get('/profile', (req, res) => {
 
+
+router.get('/profile', (req, res) => {
+    // console.log(req.headers.authorization);
+    const { authorization } = req.headers
+    try{
+        jwt.verify(authorization, secretKey);
+        res.json({data: mockUser});
+    }
+    catch(err){
+        res.send(err.message);
+    }
 });
 
 module.exports = router;
